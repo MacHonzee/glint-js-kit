634378ab2c39df0d70c7324bcffe25804fb15f94
Jan Rudolf <jan.rudolf@plus4u.net>
2025-09-22 16:10:57 +0200

    Removed all test-utils and all glint-js related code and dependencies, in effort to make it fully glint-js independent. The package should not have any dependency on glint-js, because it would bring cyclic dependency, which is very ugly in case of Npm vulnerabilities.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 import start from "./src/scripts/start.js";
-import AbstractTestUsers from "./src/test-utils/test-users.js";
-import TestService from "./src/test-utils/test-service.js";
-import AssertionService from "./src/test-utils/assertion-service.js";
 
-export { start, AbstractTestUsers, TestService, AssertionService };
+export { start };
